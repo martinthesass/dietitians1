263ed7fcf9a3333c4fb8469c66d0c7e61c855b80
martin cosgrove <martin.cosgrove55@gmail.com>
2023-02-12 10:37:12 +0000

    images compress

--- diet.js
+++ diet.js
@@ -41,3 +41,20 @@ navBtn.addEventListener("click", function(){
           navbar.classList.remove("navbar-fixed");
         }
       });
+
+
+      import imagemin from 'imagemin';
+import imageminJpegtran from 'imagemin-jpegtran';
+import imageminPngquant from 'imagemin-pngquant';
+
+const files = await imagemin(['images/*.{jpg,png}'], {
+	destination: 'build/images',
+	plugins: [
+		imageminJpegtran(),
+		imageminPngquant({
+			quality: [0.6, 0.8]
+		})
+	]
+});
+
+console.log(files);
