d464b2e0a421155727f665ee06d6d7dac93327fd
martin cosgrove <martin.cosgrove55@gmail.com>
2023-02-13 06:17:08 +0000

    commit

--- diet.js
+++ diet.js
@@ -43,18 +43,4 @@ navBtn.addEventListener("click", function(){
       });
 
 
-      import imagemin from 'imagemin';
-import imageminJpegtran from 'imagemin-jpegtran';
-import imageminPngquant from 'imagemin-pngquant';
-
-const files = await imagemin(['images/*.{jpg,png}'], {
-	destination: 'build/images',
-	plugins: [
-		imageminJpegtran(),
-		imageminPngquant({
-			quality: [0.6, 0.8]
-		})
-	]
-});
-
-console.log(files);
+      
